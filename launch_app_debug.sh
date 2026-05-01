d5fefd93105c47a85c294bdea1aad8678810d3f2
dillonhaughton <dillon.richard.haughton@gmail.com>
2026-05-01 07:17:17 -0700

    Cleaning up the repo

--- launch_app_debug.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Launching Searches.app with console output..."
-echo "=============================================="
-/Users/dillonhaughton/Documents/Searches/dist/Searches.app/Contents/MacOS/Searches
